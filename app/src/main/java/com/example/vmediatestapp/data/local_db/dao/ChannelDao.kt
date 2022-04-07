d18e8831e83ac3393e7cd0d4ddbb82be5bb8b8aa
kudaibergen_abdyldaev <abdyldaevk02@gmail.com>
2022-04-07 13:15:44 +0600

    created dao for channels

---
+++ app/src/main/java/com/example/vmediatestapp/data/local_db/dao/ChannelDao.kt
@@ -0,0 +1,17 @@
+package com.example.vmediatestapp.data.local_db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.vmediatestapp.data.local_db.model.ChannelEntity
+
+@Dao
+interface ChannelDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertChannels(channel: List<ChannelEntity>)
+
+    @Query("SELECT * FROM channel")
+    fun getPopularMovie(): List<ChannelEntity>
+}
