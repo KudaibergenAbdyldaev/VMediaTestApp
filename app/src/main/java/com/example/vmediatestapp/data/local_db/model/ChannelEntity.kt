336e823b5a2be42b47bc864cbc9d2008be099b48
kudaibergen_abdyldaev <abdyldaevk02@gmail.com>
2022-04-07 14:10:53 +0600

    added primary key

--- app/src/main/java/com/example/vmediatestapp/data/local_db/model/ChannelEntity.kt
+++ app/src/main/java/com/example/vmediatestapp/data/local_db/model/ChannelEntity.kt
@@ -1,11 +1,12 @@
 package com.example.vmediatestapp.data.local_db.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "channel")
 data class ChannelEntity(
     val orderNum: Int,
     val accessNum: Int,
     val callSign: String,
-    val id: Int
+    @PrimaryKey val id: Int
 )
