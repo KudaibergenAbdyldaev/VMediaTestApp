51bf897edbe2a4e9d5fe41f8386ddfa60fe8e823
kudaibergen_abdyldaev <abdyldaevk02@gmail.com>
2022-04-07 14:36:25 +0600

    initialize component

--- app/src/main/java/com/example/vmediatestapp/presentation/App.kt
+++ app/src/main/java/com/example/vmediatestapp/presentation/App.kt
@@ -1,11 +1,12 @@
 package com.example.vmediatestapp.presentation
 
 import android.app.Application
+import com.example.vmediatestapp.di.DaggerAppComponent
 
 class App:Application() {
 
     val component by lazy {
-        DaggerApplicationComponent.factory().create(this)
+        DaggerAppComponent.factory().create(this)
     }
 
     override fun onCreate() {
