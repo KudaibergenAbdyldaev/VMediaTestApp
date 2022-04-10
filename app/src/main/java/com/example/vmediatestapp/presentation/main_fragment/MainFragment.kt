a50852543940563dec612c57f8877076d753f5f7
kudaibergen_abdyldaev <abdyldaevk02@gmail.com>
2022-04-10 13:11:37 +0600

    call to channels api

--- app/src/main/java/com/example/vmediatestapp/presentation/main_fragment/MainFragment.kt
+++ app/src/main/java/com/example/vmediatestapp/presentation/main_fragment/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.vmediatestapp.presentation.main_fragment
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.vmediatestapp.R
+import com.example.vmediatestapp.presentation.App
+import com.example.vmediatestapp.presentation.ViewModelFactory
+import javax.inject.Inject
 
 class MainFragment : Fragment() {
 
@@ -14,7 +18,19 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
+    private val component by lazy {
+        (requireActivity().application as App).component
+    }
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
     private lateinit var viewModel: MainViewModel
+
+    override fun onAttach(context: Context) {
+        component.inject(this)
+        super.onAttach(context)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +41,7 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
     }
 
 }
