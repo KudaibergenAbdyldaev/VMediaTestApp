0a4717135c9c053c641faec045515d794bec0842
kudaibergen_abdyldaev <abdyldaevk02@gmail.com>
2022-04-10 13:26:39 +0600

    added viewBinding

---
+++ app/src/main/java/com/example/vmediatestapp/presentation/main_fragment/MainAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.vmediatestapp.presentation.main_fragment
+
+
+import androidx.recyclerview.widget.ListAdapter
+import java.nio.channels.Channel
+
+class MainAdapter: ListAdapter<Channel, > {
+
+
+    class MainAdapterViewHolder()
+
+}
